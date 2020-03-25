13430a5e67b1f83e7345e48b78edf6535967aa1d
aakash sangwan <aakashsangwan024@gmail.com>
2020-03-25 18:16:44 +0530

    basic frontend

--- server/models/User.js
+++ server/models/User.js
@@ -1,25 +1,25 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const UserSchema= new Schema({
-    first_name:{
-        type:String
+const UserSchema = new Schema({
+    first_name: {
+        type: String
     },
-    last_name:{
-        type:String
+    last_name: {
+        type: String
     },
-    email:{
-        type:String,
-        required:true   
+    email: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    date:{
-        type:Date,
-        default:Date.now
+    date: {
+        type: Date,
+        default: Date.now
     }
 })
 
-module.exports = Users =mongoose.model('users',UserSchema)
+module.exports = User = mongoose.model('users', UserSchema)
